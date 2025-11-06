7f1735c3c0f3c9b15d464391fb39e923ece8a0aa
Hampus Thalin <hampus2002@hotmail.se>
2025-11-06 11:43:47 +0100

    Start_project

---
+++ chatklient/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace chatklient;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // Vi ansluter till Socket servern.
+        await SocketManager.Connect();
+            
+        while (true)
+        {
+
+        }
+    }
+}
